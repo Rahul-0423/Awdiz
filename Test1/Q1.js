37d68976700dc6e38edf321a7c3a1a3d48a9d6ad
Rahul-0423 <rahulbiradar9987@gmail.com>
2023-08-26 18:36:04 +0530

    test

---
+++ Test1/Q1.js
@@ -0,0 +1,35 @@
+//function that takes an array of strings as input and returns a new array containing the lengths of those strings
+
+
+// let array = ["Raj","Rohit","aman","dinesh"]
+// console.log(array)
+// function stringArray(array) {
+//     for (var i; i < array.length ; i++) {
+//         result = console.log(array[i], "length of Array" )
+//         return result;
+//     }
+// }
+// console.log(array.length)
+
+
+//js function that takes an array of numbers as input and returns a new array with only the even numberrs from the orginal array.
+
+// let numberArr = [1,12, 54,11,19,75,22,65,43,254,365,34]
+// // console.log(numbArr)
+
+// function numbers(numbArr){
+//     let nArray= []
+//     for(var i=0; i < numbArr.length; i++){
+//         if(numbArr[i] % 2 == 0){
+//             nArray.push(numbArr[i])
+//             console.log(nArray)
+
+//         }
+//     }
+// }
+// numbers(numberArr)
+
+// function that takes an array of numbers as input of numbers and returns the smallest number from the array
+// const array=[98,52,45,6,75,48,32,3,44]
+// const result=Math.min(98,52,45,6,75,48,32,3,44)
+// console.log(result)
